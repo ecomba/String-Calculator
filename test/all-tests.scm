f418a030e215a77239e8bc40f9a1cf0ddc15ea15
Enrique Comba Riepenhausen <ecomba@nexwerk.com>
2009-12-19 20:34:28 +0000

    Initial Setup

---
+++ test/all-tests.scm
@@ -0,0 +1,10 @@
+; Testing template for running the tests (aka. suite)
+; 
+; author: Enrique Comba Riepenhausen
+; ecomba.github.com
+;
+(load "lib/test-manager/load.scm")
+;
+; To test add the test classes below
+;
+(load "test/string-calculator.scm")
